4de39a7bbabeeedfb792a5b03fcdf2e69bbe01b9
TamaraBermudez <tamara.bermudez.vieras@gmail.com>
2026-05-19 09:24:24 +0200

    #1 commit texto descriptivo img en dropPhoto

--- src/main/java/view/DropPhotoListener.java
+++ src/main/java/view/DropPhotoListener.java
@@ -24,8 +24,7 @@ public class DropPhotoListener implements DropTargetListener {
 
     JDialog jDialog;
     JLabel label;
-    String dragText = "<html><center>PHOTO</center></br><br><center> <i>Supported format: PNG.</i></center></br><br><center><i>Max. size 64KB</i></center></html>";
-
+    String dragText = "<html><center>Deja tu archivo aquí</center><br><center>PHOTO</center><br><center><i>Supported format: PNG.</i></center><br><center><i>Max size 64KB</i></center></html>";
     public DropPhotoListener(JLabel label, JDialog jDialog) {
         this.label = label;
         this.jDialog = jDialog;
