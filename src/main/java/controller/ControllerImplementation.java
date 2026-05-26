e23f8f1d5baa6e170717cf9d0beb1031ac9b2c5d
Eduard-Reineth <eduard.reineth@gmail.com>
2026-05-26 10:07:59 +0200

    Replace Hardcoded Strings with Constants in Constants Class #2

--- src/main/java/controller/ControllerImplementation.java
+++ src/main/java/controller/ControllerImplementation.java
@@ -37,6 +37,7 @@ import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import org.jdatepicker.DateModel;
+import utils.Constants;
 
 /**
  * This class starts the visual part of the application and programs and manages
@@ -119,22 +120,22 @@ public class ControllerImplementation implements IController, ActionListener {
         String daoSelected = ((javax.swing.JCheckBox) (dSS.getAccept()[1])).getText();
         dSS.dispose();
         switch (daoSelected) {
-            case "ArrayList":
+            case Constants.ArrayList:
                 dao = new DAOArrayList();
                 break;
-            case "HashMap":
+            case Constants.HashMap:
                 dao = new DAOHashMap();
                 break;
-            case "File":
+            case Constants.File:
                 setupFileStorage();
                 break;
-            case "File (Serialization)":
+            case Constants.File_Serialization:
                 setupFileSerialization();
                 break;
-            case "SQL - Database":
+            case Constants.SQL_Database:
                 setupSQLDatabase();
                 break;
-            case "JPA - Database":
+            case Constants.JPA_Database:
                 setupJPADatabase();
                 break;
         }
